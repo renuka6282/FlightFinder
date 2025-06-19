83509be24e0580c5c983a44d0b6f48a51d57be81
sai rohith <22pa1a1285@vishnu.edu.in>
2025-06-19 16:27:39 +0530

    added mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,18 @@
-//Connects MongoDB using Mongoose
+// db.js
+const mongoose = require('mongoose');
+require('dotenv').config(); 
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("mongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-//Entry point (starts Express server)
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB(); 
+
+
+
+app.listen(3000, () => {
+  console.log(" Server running on http://localhost:3000");
+});
